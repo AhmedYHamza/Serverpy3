83155a14f6b5a1d98e470518915b362701963aab
AhmedYHamza <66231209+AhmedYHamza@users.noreply.github.com>
2020-06-01 20:32:26 +0200

    Update ftpserverpy3.py

--- ftpserverpy3.py
+++ ftpserverpy3.py
@@ -1,9 +1,11 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+from os.path import expanduser
+home = expanduser("~")
 authorizer = DummyAuthorizer()
-authorizer.add_user("user", "12345", "C:/Server/user", perm="elradfmw")
-authorizer.add_anonymous("C:/Server/user", perm="elradfmw")
+authorizer.add_user("user", "12345", home+"/user", perm="elradfmw")
+authorizer.add_anonymous(home+"/user", perm="elradfmw")
 handler = FTPHandler
 handler.authorizer = authorizer
 server = FTPServer(('localhost'), handler)
