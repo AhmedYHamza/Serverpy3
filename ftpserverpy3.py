4292c0ce9a186c7ca0915176e5e77b9806fe5eb5
AhmedYHamza <66231209+AhmedYHamza@users.noreply.github.com>
2020-06-01 20:43:41 +0200

    Update ftpserverpy3.py

--- ftpserverpy3.py
+++ ftpserverpy3.py
@@ -3,12 +3,19 @@ from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 from os.path import expanduser
 import os
-home = expanduser("~")
+import socket
+from contextlib import closing
+
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]home = expanduser("~")
 os.mkdir(home+"/user")
 authorizer = DummyAuthorizer()
 authorizer.add_user("user", "12345", home+"/user", perm="elradfmw")
 authorizer.add_anonymous(home+"/user", perm="elradfmw")
 handler = FTPHandler
 handler.authorizer = authorizer
-server = FTPServer(("127.0.0.1", 21), handler)
+server = FTPServer(("127.0.0.1", find_free_port()), handler)
 server.serve_forever()
