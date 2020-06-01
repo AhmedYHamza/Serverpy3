6a420a2b70772cfd7853626a1006fb935d36ccbc
AhmedYHamza <66231209+AhmedYHamza@users.noreply.github.com>
2020-06-01 20:38:25 +0200

    Update ftpserverpy3.py

--- ftpserverpy3.py
+++ ftpserverpy3.py
@@ -10,5 +10,5 @@ authorizer.add_user("user", "12345", home+"/user", perm="elradfmw")
 authorizer.add_anonymous(home+"/user", perm="elradfmw")
 handler = FTPHandler
 handler.authorizer = authorizer
-server = FTPServer(('localhost'), handler)
+server = FTPServer(("127.0.0.1", 21), handler)
 server.serve_forever()
